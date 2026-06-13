ba2f98fca1764aab8b8e92f775c6fe7840a2afd6
Tomas <tomasgomezzi36@gmail.com>
2026-06-12 23:08:10 -0300

    feat: (frontend) Elimina la importación de RegisterPage y limpie el enrutamiento en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
-import Login from './pages/login/Login.jsx'
+import Login from './pages/login/Login.jsx';
 import { AuthProvider } from "./context/AuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
+import StaffManagement from './pages/admin/StaffManangement.jsx';
 
 function App() {
   return (
@@ -12,12 +12,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* Rutas privadas */}
+        
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/profile" element={<ProfilePage />} />
           </Route>
+          <Route element={<ProtectedRoute allowedRoles={['profesor']} />}>
+          </Route>
+          <Route element={<ProtectedRoute allowedRoles={['super_adm']} />}>
+            <Route path="/staff" element={<StaffManagement />} />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
